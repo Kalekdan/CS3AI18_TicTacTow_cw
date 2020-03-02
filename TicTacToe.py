5b41cc8cc1da1081d28e0c42c0a1b4988de8c021
kalekdan <joerickard@hotmail.co.uk>
2020-03-02 14:36:56 +0000

    Moving to python3.6 and importing tkinter

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,18 @@
 from gameboard import gameboard
 from players import HumanPlayer
+from tkinter import *
+from tkinter import ttk
+
 
 continuePlaying = False
+window = Tk()
+window.title("Tic Tac Toe Game")
+
+mainframe = ttk.Frame(window, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
+window.mainloop()
 
 while True:
     board = gameboard()
---
+++ venv/Scripts/easy_install-3.6-script.py
@@ -0,0 +1,12 @@
+#!"N:\Documents\Part 3\CS3AI18\TicTacToe_cw\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.0.1','console_scripts','easy_install-3.6'
+__requires__ = 'setuptools==39.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.0.1', 'console_scripts', 'easy_install-3.6')()
+    )
